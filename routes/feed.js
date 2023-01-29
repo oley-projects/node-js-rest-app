25ced3275863252080df632ad777c3580edd2dd9
Devole <oley.for.git@gmail.com>
2023-01-29 18:02:50 +0200

    server side validation

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
@@ -8,6 +9,13 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post('/post', [
+  body('title')
+    .trim()
+    .isLength({ min: 5, max: 40 }),
+  body('content')
+    .trim()
+    .isLength({ min: 5, max: 300 })
+], feedController.createPost);
 
 module.exports = router;
