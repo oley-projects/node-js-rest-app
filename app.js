3d0af9a1c2b8107a949c56f7dcc0eb3d94b385e0
Devole <oley.for.git@gmail.com>
2023-02-01 15:54:42 +0200

    connect to a database, mongoose

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config()
 
 const feedRoutes = require('./routes/feed');
 
-const app = express();
+const MONGODB_URI = dotenv.parsed.MONGODB_URI;
 
+const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGODB_URI)
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
