135a64e1756abbf6384280df0ab05aa3923fd486
Devole <oley.for.git@gmail.com>
2023-02-08 19:32:20 +0200

    update post controller

--- routes/feed.js
+++ routes/feed.js
@@ -9,15 +9,31 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', [
-  body('title')
-    .trim()
-    .isLength({ min: 5, max: 40 }),
-  body('content')
-    .trim()
-    .isLength({ min: 5, max: 300 })
-], feedController.createPost);
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5, max: 40 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5, max: 300 })
+  ],
+  feedController.createPost);
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5, max: 40 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5, max: 300 })
+  ],
+  feedController.updatePost
+);
 
 module.exports = router;
