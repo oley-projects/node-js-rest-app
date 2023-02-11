619b786d7c8e0bcfc492ca3721b7dcba32ff2b51
Devole <oley.for.git@gmail.com>
2023-02-11 23:04:55 +0200

    user controller, bcrypt password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
-const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -12,4 +14,22 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
+  bcrypt.hash(password, 12)
+    .then(hashedPwd => {
+      const user = new User({
+        email,
+        password: hashedPwd,
+        name
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({message: 'User created.', userId: result._id});
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
   },
   status: {
     type: String,
-    default: 'I am new'
+    default: 'I am new!'
   },
   posts: [{
     type: Schema.Types.ObjectId,
