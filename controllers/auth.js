2e43dfd1f8dd01aac991dbbc2393e1eaa5e13c4d
Devole <oley.for.git@gmail.com>
2023-02-13 16:03:23 +0200

    login function, jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -32,4 +33,44 @@ exports.signup = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        const error = new Error('An user is not exist.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Wrong password');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        'secretKey',
+        {
+          expiresIn: '2h'
+        }
+      );
+      res.status(200).json({ token, userId: loadedUser._id.toString() })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    })
 };
